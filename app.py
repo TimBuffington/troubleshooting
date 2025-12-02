8783902faefd2a75436ee92a61f8f337a54bb31a
Tim Buffington <timbuffington@gmail.com>
2025-12-02 04:34:30 -0800

    Update app.py

--- app.py
+++ app.py
@@ -248,8 +248,8 @@ header {
 """, unsafe_allow_html=True)
 
 st.markdown("<div class='line1'>EBOSS® Fault Code Lookup </div>", unsafe_allow_html=True)
-
-st.markdown("<div class='line1'>Select the drive, enter the fault code (e.g., F91), then Search.</div>", unsafe_allow_html=True)st.write("")
+st.markdown("<div class='line1'>Select the drive, enter the fault code (e.g., F91), then Search.</div>", unsafe_allow_html=True)
+st.write("")
 
 # ============================================================
 # 🟦 BLUE — Drive / Fault Code / Search
