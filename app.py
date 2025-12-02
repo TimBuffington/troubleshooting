84a8b00bf7fbb78fef289d57aed003580e6c03d8
Tim Buffington <timbuffington@gmail.com>
2025-12-02 04:47:45 -0800

    Adjust header font size and fix HTML attribute

--- app.py
+++ app.py
@@ -235,11 +235,12 @@ def show_result(entry: dict):
 # ----------------------------
 # Header
 # ----------------------------
+
 st.markdown("""
 <style>
 .Header {
     text-align: center;
-    font-size: 4rem;
+    font-size: 3.0rem;
     font-weight: bold;
     color: white;
     text-shadow: 2px 2px 4px #000; /* simple dark shadow */
@@ -247,7 +248,7 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
-st.markdown("<div class='page_title'>EBOSS® Fault Code Lookup</div>", unsafe_allow_html=True)
+st.markdown("<div class='page_title='>EBOSS® Fault Code Lookup</div>", unsafe_allow_html=True)
 
 st.markdown("<div class='muted'>Select the drive, enter the fault code (e.g., F91), then Search.</div>", unsafe_allow_html=True)
 st.write("")
